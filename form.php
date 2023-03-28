2007357a7ad600300a2400b600c05eafc46fb350
VldimrKulikov <112845661+VldimrKulikov@users.noreply.github.com>
2023-03-28 22:39:52 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <form action="" method="POST">
+	FIO:
   <input name="fio" />
   <select name="year">
     <?php 
@@ -7,48 +8,51 @@
     }
     ?>
   </select>
-			<br />
+			<br />	
+			<p>
+	<label>
+		Напишите биографию:<br />
+		<textarea name="biography">начальное значение</textarea>
+	</label><br /><p\>
 			<label>
 				Email
 				<br />
-				<input name="field-email-1" value="Введите почту">
+				<input name="email" value="Введите почту">
 			</label>
-			Выберите пол:<br />
-      <label><input type="radio" checked="checked"
-        name="radio-group-1" value="Значение1" />
-        Мужской</label>
-      <label><input type="radio"
-        name="radio-group-1" value="Значение2" />
-        Женский</label><br />
+			</p>
+
 				Кол-во конечностей:<br />
 				<label><input type="radio" checked="checked"
-					name="radio-group-1" value="Значение1" />
+					name="limbs" value="1" />
 					 1</label>
 				<label><input type="radio"
-					name="radio-group-1" value="Значение2" />
+					name="limbs" value="2" />
 					 2</label>
 					 <label><input type="radio"
-						name="radio-group-1" value="Значение3" />
+						name="limbs" value="3" />
 						 3</label>
 						 <label><input type="radio"
-							name="radio-group-1" value="Значение4" />
+							name="limbs" value="4" />
 							 4</label><br />
+							 <p>Выберите пол: <br>
+<label><input type="radio" checked="checked"
+					name="gender" value="M" />
+					 M</label>
+				<label><input type="radio"
+					name="gender" value="F" />
+					 F</label></p>
 	<label>Сверхспособности:<br />
-		<select name="field-name-4[]" multiple="multiple">
-			<option value="Значение1">Левитация</option>
-			<option value="Значение2" selected="selected">Прохождение сквозь стены </option>
-			<option value="Значение3" selected="selected">Бессмертие</option>
+		<select name="ability[]" multiple="multiple">
+			<option value="1">Левитация</option>
+			<option value="2" selected="selected">Прохождение сквозь стены </option>
+			<option value="3" selected="selected">Бессмертие</option>
 		</select>
 	</label>
-	<p>
-	<label>
-		Напишите биографию:<br />
-		<textarea name="field-name-2">начальное значение</textarea>
-	</label><br /><p\>
+
 		<p>
 			Чекбокс:<br />
       <label><input type="checkbox"
-        name="check-1" />
+        name="checkbox" />
         C контрактом ознакомлен</label><br />
 		</p>
 		<p>
